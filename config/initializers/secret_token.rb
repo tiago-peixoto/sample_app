1cf8992a5221cfcadb5c434038a0e6e8e3963db2
tiago-peixoto <tiago@plyom.com>
2013-06-03 17:44:07 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampeApp::Application.config.secret_token = '1ee9700796d86f6441da04009b91140e6df689600cdaa903fb881eac83c26b9444619b31cb18424e8ad52625ed12b0cc8cfba0bfa5807bf5c6b2e52a98a62020'
